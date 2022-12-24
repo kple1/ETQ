413e0eb880227f766089756eff6511d202a06693
Leeple <kple1@naver.com>
2022-12-25 00:16:05 +0900

    English Test Question

--- src/main/java/EnglishTestQuestion.java
+++ src/main/java/EnglishTestQuestion.java
@@ -1,26 +1,41 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
-public class EnglishTestQuestion extends JFrame{
-    JTextField a = new JTextField(20);
-    JTextArea b = new JTextArea(7, 20);
-
-    EnglishTestQuestion() {
-        setTitle("유찬이의 영어단어 시험");
-        setSize(850, 500);
-        setLocationRelativeTo(null);
-        setVisible(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        Container c = getContentPane();
-        c.setLayout(new FlowLayout());
-        c.add(new JLabel("complain"));
-        c.add(a);
-        c.add(new JLabel("work"));
-        c.add(a);
+public class EnglishTestQuestion extends JFrame {
+    public EnglishTestQuestion() throws HeadlessException {
+        initComponents();
     }
+
     public static void main(String[] args) {
-        new EnglishTestQuestion();
-
-        }
+        SwingUtilities.invokeLater(() -> new EnglishTestQuestion().setVisible(true));
     }
+
+    protected void initComponents() {
+
+        setSize(700, 400);
+        setTitle("유찬이의 영어단어 시험");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new FlowLayout(FlowLayout.LEFT));
+
+
+        JLabel ETQLABEL = new JLabel("Complain: ");
+        JTextField ETF = new JTextField();
+        ETF.setPreferredSize(new Dimension(150, 20));
+        getContentPane().add(ETQLABEL);
+        getContentPane().add(ETF);
+
+        ETF.addKeyListener(new KeyAdapter() {
+            public void keyReleased(KeyEvent e) {
+                JTextField textField = (JTextField) e.getSource();
+                String text = textField.getText();
+                if (text == "asd") {
+                    ETF.setBorder(BorderFactory.createLineBorder(Color.GREEN));
+                } else {
+                    ETF.setBorder(BorderFactory.createLineBorder(Color.RED));
+                }
+            }
+        });
+    }
+}
